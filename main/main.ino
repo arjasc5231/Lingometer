b176451c414a204ac1be88db35463bbfbd06003f
Baekho <0110kim20@kaist.ac.kr>
2022-10-28 00:39:39 +0900

    PT완료

--- main/main.ino
+++ main/main.ino
@@ -28,7 +28,7 @@ short Buffer[256]; // 음성 신호 입력받을 변수
 volatile int Read; //음성 신호 입력용 변수
 
 pt ptState;
-int StateThread(struct pt* pt){
+int stateThread(struct pt* pt){
   PT_BEGIN(pt);
   for(;;){
     button1_chk=digitalRead(2);
@@ -38,45 +38,104 @@ int StateThread(struct pt* pt){
     PT_END(pt);
   }
 
-pt ptButton1Time; // 버튼1 프레스 시간 확인용 PT
+pt ptButton1Time; // 버튼1 프레스 시간 확인 및 조작용 PT
 int button1TimeThread(struct pt* pt){
   PT_BEGIN(pt);
   for(;;){
       PT_YIELD(pt);
+      
       PT_WAIT_UNTIL(pt, button1_chk==0);
       b1_in_time=millis();
       last_control=millis();
       Serial.println("button1ininininin");
       PT_YIELD(pt);
+      
       PT_WAIT_UNTIL(pt,button1_chk==1);
       b1_out_time=millis();
       last_control=millis();
+      light=1;
+      
+      if(b1_out_time-b1_in_time<2000){
+        if(mode==0){mode=1;}
+        else if(mode=1){mode=0;} //짧게 누르면 측정 일시정지/ 재개하고
+        } else {num_words=0;} //길게 누르면 측정 초기화한다.
       Serial.println("button1outoutoutoutout");
       PT_YIELD(pt);
     }
   PT_END(pt)
   }
 
-pt ptButton2Time; // 버튼2 프레스 시간 확인용 PT
+pt ptButton2Time; // 버튼2 프레스 시간 확인 및 조작용 PT
 int button2TimeThread(struct pt* pt){
   PT_BEGIN(pt);
 
   for(;;){
       PT_YIELD(pt);
+      
       PT_WAIT_UNTIL(pt, button2_chk==0);
       b2_in_time=millis();
       last_control=millis();
       Serial.println("button2ininininin");
       PT_YIELD(pt);
+      
       PT_WAIT_UNTIL(pt,button2_chk==1);
       b2_out_time=millis();
       last_control=millis();
+      
+      if(b2_out_time-b2_in_time<2000){
+        if(light==0){light=1;}
+        else if(light==1){light=0;}
+        } //짧게 누르면 불이 켜지거나 꺼지고
+        else{
+          mode=2;
+          light=1;
+          } //길게 누르면 불이 무조건 켜지면서 모드 2
       Serial.println("button2outoutoutoutout");
       PT_YIELD(pt);     
       
     }
   PT_END(pt)
   }
+
+
+pt ptCountWords; //모드1에서 단어수 세는용도 pt
+int countWordsThread(struct pt* pt){
+  PT_BEGIN(pt);
+  for(;;){
+    if(mode==1){
+      num_words=num_words+Count();
+      }
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
+  }
+
+
+pt ptDisplayNum; //모드에 맞는 디스플레이용 pt
+int displayNumThread(struct pt* pt){
+  PT_BEGIN(pt);
+  for(;;){
+    PT_YIELD(pt);
+    if(light==1){ //일단 불이 들어와있으면
+    if(mode==1){onRecording(num_words);}
+    else if(mode==0){onStop(num_words);} //모드 1, 0일때 해당하는 화면 디스플레이
+    else{
+      LearningVoice();
+      mode=1;
+      }} // 모드 2일때 학습화면 디스플레이 및, 학습해주는 함수 호출
+      else if(light==0){lightOff();} // 불 안들어와있으면 꺼줌.
+      
+      if(millis()-last_control>10000){ //마지막 조작에서 10초 이상 지나면
+        light=0;
+        lightOff(); //불 끄고
+        PT_YIELD(pt);
+        PT_WAIT_UNTIL(pt,light==1); //다시 조작할때까지(불켜질 때까지) 기다림.
+        }
+      PT_YIELD(pt);
+  }
+  PT_END(pt);
+  }
+
 
 //마이크 입력받는 함수
 void onPDMdata(){
@@ -90,6 +149,12 @@ void setup() {
     Serial.begin(9600);
     pinMode(2, INPUT_PULLUP);
     pinMode(4, INPUT_PULLUP); //조작 버튼 두 개 핀모드 설정
+
+    PT_INIT(&ptButton1Time); //PT들 시작
+    PT_INIT(&ptButton2Time);
+    PT_INIT(&ptState);
+    PT_INIT(&ptCountWords);
+    PT_INIT(&ptDisplayNum);
     
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -100,7 +165,7 @@ void setup() {
   display.clearDisplay();
   display.display();
 
-  PDM.onReceive(onPDMdata); //PDM 작동 확인
+  PDM.onReceive(onPDMdata); //PDM(음성신호) 작동 확인
   if(!PDM.begin(1,16000)){
     Serial.println("Failed to start PDM!");
     while(1);
@@ -111,7 +176,9 @@ void setup() {
 void loop() {
   PT_SCHEDULE(button1TimeThread(&ptButton1Time));
   PT_SCHEDULE(button2TimeThread(&ptButton2Time));
-  PT_SCHEDULE(StateThread(&ptState));
+  PT_SCHEDULE(stateThread(&ptState));
+  PT_SCHEDULE(countWordsThread(&ptCountWords));
+  PT_SCHEDULE(displayNumThread(&ptDisplayNum));
 
 
 /*  //버튼 조작
